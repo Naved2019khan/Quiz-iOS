33272eb50f07a623cb0b223c88cc74c820a7a7c9
Md Naved Khan <72648913+Naved2019khan@users.noreply.github.com>
2023-09-10 21:22:34 +0530

    Add files via upload

---
+++ TrivaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrivaApp
+//
+//  Created by Naved Khan on 14/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
